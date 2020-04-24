de1d51f47bc11c5285e4241ebf9b92954699d3d5
마경욱 <kyeongwook.ma@liveo.me>
2020-04-25 08:40:41 +0900

    renaming module
    - auth -> account

--- account/models.py
+++ account/models.py
@@ -7,4 +7,4 @@ from django.db import models
 class Account(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.CharField(max_length=100)
-    password = models.CharField(validators=MinLengthValidator(10))
+    password = models.CharField(validators=[MinLengthValidator(10)], max_length=100)
--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.db import models
 
 
-class Account(models.Model):
+class User(models.Model):
     class Gender(models.TextChoices):
         MALE = 'M'
         FEMALE = 'F'
