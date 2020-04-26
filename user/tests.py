aa8c657c6a302c370da0668ea0e37f42aca09750
마경욱 <kyeongwook.ma@liveo.me>
2020-04-26 23:59:27 +0900

    add filter module
    - get user by filter [name, email]

--- user/tests.py
+++ user/tests.py
@@ -10,21 +10,16 @@ class UserServiceTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         User.objects.create(id='7bc3c004-05d6-4d4d-a70b-cf0507320784', name="user1",
+                            email='sample@example.com',
                             nickname="u1", phone_number='010-0000-0000')
 
     def test_create_user_service(self):
         """
         create_user() should return False for future publication dates.
         """
-        result = services.create_user(_name='John', _nickname='Doe', _phone_number='010-0000-0000')
+        result = services.create_user(_name='John', _email='sample@example.com', _nickname='Doe',
+                                      _phone_number='010-0000-0000')
         self.assertEqual(result, True)
-
-    def test_get_user_service(self):
-        target_user = User(id='7bc3c004-05d6-4d4d-a70b-cf0507320784', name="user1",
-                           nickname="u1", phone_number='010-0000-0000')
-        result = services.get_user('7bc3c004-05d6-4d4d-a70b-cf0507320784')
-        print(target_user, result)
-        self.assertEqual(result, target_user)
 
 
 class UserAPITests(APITestCase):
@@ -35,6 +30,14 @@ class UserAPITests(APITestCase):
         response = self.client.post('/users/', format='json', data={
             'name': 'John',
             'nickname': 'Doe',
+            'email': 'sample@example.com',
             'phone_number': '010-0000-0000'
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_user_api(self):
+        """
+        Ensure we can create a new account object.
+        """
+        response = self.client.get('/users/7bc3c004-05d6-4d4d-a70b-cf0507320784')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- user/views.py
+++ user/views.py
@@ -1,9 +1,11 @@
 import phonenumbers
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework import serializers
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from user import services
@@ -28,8 +30,11 @@ class UserAPIView(mixins.RetrieveModelMixin,
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    filter_backends = (SearchFilter,)
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['name', 'email']
     search_fields = ('name', 'email')
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 10
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
